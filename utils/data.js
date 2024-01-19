79296df3b50e6294f281f8c9e747d1f22f55758d
Robert Bailey <r.bail@hotmail.com>
2024-01-20 09:02:06 +1030

    app structure

---
+++ utils/data.js
@@ -0,0 +1,32 @@
+const userSeeds = [
+    {
+        username: 'jeff1000',
+        email: 'jeff@email.com'
+    },
+    {
+        username: 'jimbo44',
+        email: 'jimbo@hotmail.com'
+    },
+    {
+        username: 'falconBird',
+        email: 'oliviah01@gmail.com'
+    }
+]
+
+const thoughtSeeds = 
+    [{
+        thoughtText: 'Cillian Murphy sounds like Robin Williams in Oppenheimer',
+        username: 'jeff1000'
+    },
+    {
+        thoughtText: 'Sunflowers are an underrated gift',
+        username: 'jimbo44'
+    },
+    {
+        thoughtText: 'I like snakes and spiders',
+        username: 'falconBird'
+    }
+]
+
+
+module.exports = { userSeeds, thoughtSeeds };
